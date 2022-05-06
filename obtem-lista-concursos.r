113461954fff9c8690eac07936f41ff854104d24
Jackson Assis <jackson.assis07@gmail.com>
2022-05-05 21:33:36 -0300

    obtencao da lista de concursos

---
+++ obtem-lista-concursos.r
@@ -0,0 +1,23 @@
+# configuracoes ================================================================
+rm(list = ls())
+
+if (!require(rvest)) install.packages(rvest)
+library(rvest)
+#===============================================================================
+
+url = 'https://www.pciconcursos.com.br/concursos/nacional/'
+urls_regioes = url %>%
+  read_html() %>%
+  html_nodes('.regioes') %>%
+  html_nodes('a') %>%
+  html_attr('href')
+
+res = sapply(urls_regioes, \(url) {
+  url %>%
+    read_html() %>%
+    html_nodes('.ca') %>%
+    html_nodes('[rel="bookmark"]') %>%
+    html_attr('href')
+})
+
+saveRDS(res, 'lista-urls-concursos.rds')
